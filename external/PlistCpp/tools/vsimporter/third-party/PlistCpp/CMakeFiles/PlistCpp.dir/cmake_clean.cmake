7f4a13217cf761e4ed9984e12d9a7a6d0a6269a8
dkimitsa <demyan.kimitsa@gmail.com>
2017-11-22 10:46:14 +0200

    Added as separate external dependency to allow inplace modifications

---
+++ external/PlistCpp/tools/vsimporter/third-party/PlistCpp/CMakeFiles/PlistCpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PlistCpp.dir/src/Plist.cpp.o"
+  "CMakeFiles/PlistCpp.dir/src/PlistDate.cpp.o"
+  "libPlistCpp.pdb"
+  "libPlistCpp.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PlistCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
